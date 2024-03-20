ddb29f31677fe727ca56e77c6825dd8836bad450
Charles <chcar264@ulaval.ca>
2024-03-19 21:30:23 -0400

    deuxième soumission

--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,8 +1,11 @@
+ALTER TABLE musician DROP FOREIGN KEY musician_ibfk_1;
+
+ALTER TABLE musician
+DROP COLUMN role,
+DROP COLUMN bandName;
+
+ALTER TABLE musician CHANGE COLUMN musicianName singerName varchar(50);
 
 ALTER TABLE musician RENAME TO singer;
-ALTER TABLE singer DROP COLUMN role;
-ALTER TABLE singer DROP COLUMN bandName;
+
 DROP TABLE IF EXISTS band;
-ALTER TABLE singer CHANGE musicianName singerName VARCHAR(50);
-ALTER TABLE album DROP FOREIGN KEY FK_A_musicianName;
-ALTER TABLE album ADD CONSTRAINT FK_A_singerName FOREIGN KEY (singerName) REFERENCES singer(singerName);
